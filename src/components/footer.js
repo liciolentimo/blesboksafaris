5fbbfa9d1c9506530991c6052e5e67923b52d597
liciolentimo <lentimo@gmail.com>
2020-11-23 11:24:10 +0300

    added footer

---
+++ src/components/footer.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Link} from 'gatsby'
+
+const Footer = () => {
+    return (
+    <FooterContainer>
+        <FooterLinksWrapper>
+            <FooterDesc>
+                <h1>Blesbok Safaris</h1>
+                <p>Blesbok Safaris is a Kenyan Company that provides Safari Tours and Travel.We aim at providing you with the best safari experience in Kenya. Enjoy the great wildlife and marine parks Kenya has to offer. Book with us today!</p>
+
+            </FooterDesc>
+            <FooterLinkItems>
+                <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                <FooterLink to="/contact">Contact</FooterLink>
+                <FooterLink to="/">Support</FooterLink>
+                <FooterLink to="/destinations">Destinations</FooterLink>
+            </FooterLinkItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
+        <FooterLinkItems>
+                <FooterLinkTitle>Social Media</FooterLinkTitle>
+                <FooterLink to="/">Facebook</FooterLink>
+                <FooterLink to="/">Instagram</FooterLink>
+                <FooterLink to="/">Twitter</FooterLink>
+            </FooterLinkItems>
+        </FooterLinksWrapper>
+    </FooterContainer>
+    )
+}
+
+export default Footer
+
+const FooterContainer = styled.div `
+padding: 5rem calc((100vw - 1100px) / 2);
+display: grid;
+grid-template-columns: repeat(2, 1fr);
+color: #000;
+background: #fafafb;
+`
+
+const FooterDesc = styled.div `
+padding: 0 2rem;
+h1{
+    margin-bottom: 3rem;
+    color: #f26a2e;
+}
+@media screen and (max-width: 400px){
+    padding: 1rem;
+}
+`
+
+const FooterLinksWrapper = styled.div `
+display: grid;
+grid-template-columns: repeat(2, 1fr);
+@media screen and (max-width: 820px){
+    grid-template-columns: 1fr;
+}
+`
+
+const FooterLinkItems = styled.div `
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 1rem 2rem;
+@media screen and (max-width: 400px){
+    padding: 1rem;
+}
+`
+
+const FooterLinkTitle = styled.h2 `
+font-size: 14px;
+margin-bottom: 16px;
+`
+
+const FooterLink = styled(Link) `
+text-decoration: none;
+margin-bottom: 0.5rem;
+font-size: 14px;
+color: #3d3d4e;
+&:hover{
+    color: #f26a2e;
+    transition: 0.3s ease-out;
+}
+`
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Footer from "./footer"
 import Header from "./header"
 import { GlobalStyle } from "./styles/globalstyles"
 
@@ -11,7 +12,7 @@ const Layout = ({ children }) => {
       <Header />
     
         <main>{children}</main>
-        
+        <Footer />
     </>
   )
 }
